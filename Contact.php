dd6e7d6d5834cf26baddd2991854c0b6216abb39
CatBakun <eabakun@gmail.com>
2015-08-17 14:45:23 -0300

    email

--- Contact.php
+++ Contact.php
@@ -5,14 +5,17 @@ class Contact extends CI_Controller {
         {
             $this->load->library('email');
 
-            $this->email->from('your@example.com', echo $this->input->post('nombre'));
+            $this->email->from('contacto@conceptonea.com.ar', 'Concepto NEA');
             $this->email->to(echo $this->input->post('email'));
-            $this->email->cc(echo $this->input->post('email'));
-            //$this->email->bcc('them@their-example.com');
+            $this->email->subject('Mensaje recibido');
+            $this->email->message(echo 'Gracias por contactarse con Concepto NEA. Nos comunicaremos a la brevedad.');
+            $this->email->send();
 
-            $this->email->subject('Mensaje de contacto en entorno-pruebas.com');
-            $this->email->message(echo $this->input->post('mensaje'));
-
+            $this->email->from(echo $this->input->post('email'), echo $this->input->post('nombre'));
+            $this->email->to('contacto@conceptonea.com.ar');
+            $this->email->subject('Mensaje desde formulario de contacto');
+            $mensaje = echo "Mensaje recibido \nNombre: ". $this->input->post('nombre') . "\n:Email: " . $this->input->post('email') . "\nMensaje: " . $this->input->post('mensaje') . "\nTeléfono: " . $this->input->post('telefono');
+            $this->email->message($mensaje);
             $this->email->send();
 
             echo 200;
